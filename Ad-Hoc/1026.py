fe21bd28eb953e63e0bc2dd47b374af28ee74c15
Saymon94 <sauloroberto1000@hotmail.com>
2022-04-15 19:28:39 -0300

    problema 1026 melhorado

--- Ad-Hoc/1026.py
+++ Ad-Hoc/1026.py
@@ -1,23 +1,19 @@
-num1, num2 = ("{0:b}".format(int(numero)) for numero in input().split())
+num1, num2 = ("{0:b}".format(int(numero))[::-1] for numero in input().split())
 
 while True:
     if len(num1) > len(num2):
-        num2 = ('0' * (len(num1) - len(num2)) + num2)[::-1]
-        num1 = num1[::-1]
+        num2 = (num2 + '0' * (len(num1) - len(num2)))
     else:
-        num1 = ('0' * (len(num2) - len(num1)) + num1)[::-1]
-        num2 = num2[::-1]
+        num1 = (num1 + '0' * (len(num2) - len(num1)))
 
     numero = 0
-    i = 0
-    while i < len(num1):
+    for i in range(len(num1)):
         if num1[i] != num2[i]:
             numero += 2**i
-        i += 1
     print(numero)
 
     # EOFError
     try:
-        num1, num2 = ("{0:b}".format(int(numero)) for numero in input().split())
+        num1, num2 = ("{0:b}".format(int(numero))[::-1] for numero in input().split())
     except EOFError:
         break
