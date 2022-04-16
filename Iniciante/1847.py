b33f233ddd264b4dd5678db7b4681ed6eea59b55
Saymon94 <sauloroberto1000@hotmail.com>
2022-04-16 20:30:48 -0300

    problema 1847 resolvido

--- Iniciante/1847.py
+++ Iniciante/1847.py
@@ -3,7 +3,18 @@ t1, t2, t3 = map(int, input().split())
 var1 = t2 - t1
 var2 = t3 - t2
 
-if var2 > var1:
-    print(":)")
+if var1 > 0:
+    if var2 >= var1:
+        print(":)")
+    else:
+        print(":(")
+elif var1 < 0:
+    if var2 > var1:
+        print(":)")
+    else:
+        print(":(")
 else:
-    print(":(")
+    if var2 > 0:
+        print(":)")
+    else:
+        print(":(")
