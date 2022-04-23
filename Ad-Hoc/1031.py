f7baee83b823898182db19543ad8d3620e136e4b
saulorcj <sauloroberto1000@hotmail.com>
2022-04-23 15:16:05 -0300

    Atualização do problema 1031

--- Ad-Hoc/1031.py
+++ Ad-Hoc/1031.py
@@ -1,30 +1,27 @@
 qut_regiao = int(input())
-lista_principal = []
 
-while qut_regiao != 0:
-    lista = []
-    i = 0
-    while i < qut_regiao:
-        lista.append(i+1)
-        i += 1
-    res = False
-    salto = 1
-    while not res:
-        lista_sub = list(lista)
-        i = 0
-        while len(lista_sub) > 1:
-            if i + salto > len(lista_sub) -1:
-                del(lista_sub[i])
-                i = i - 1 + salto
-                while i > len(lista_sub) - 1:
-                    i -= len(lista_sub)
-            else:
-                del(lista_sub[i])
-                i += salto - 1
-        if lista_sub[0] == 13:
-            res = True
-            lista_principal.append(salto)
+while qut_regiao:
+    # Lista de areas a ser copiada
+    lista = list(range(1, qut_regiao + 1))
+    # Salto a ser iterado
+    salto = 0
+    while True:
+        # Lista de areas a ser manipulada
+        areas = list(lista)
         salto += 1
+        i = 0
+        while len(areas) > 1:
+            # Caso a area 13 não seja a última eliminada
+            if areas[i] == 13:
+                break
+            # Caso o salto ultrapasse o tamanho da lista
+            if i + salto > len(areas) - 1:
+                del(areas[i])
+                i = (i - 1 + salto) % len(areas)
+            else:
+                del(areas[i])
+                i = i - 1 + salto
+        if len(areas) == 1:
+            print(salto)
+            break
     qut_regiao = int(input())
-for i in lista_principal:
-    print(i)
