45c68df11de9546fa4967558384a8ceb019b404e
saulorcj <sauloroberto1000@hotmail.com>
2022-05-02 00:27:54 -0300

    Mais algumas pequenas mudanças

--- Não Resolvido/1023.py
+++ Não Resolvido/1023.py
@@ -1,36 +1,26 @@
 from collections import defaultdict
 
 qt_imoveis = int(input())
-lista_prints = []
-index = 1
+i_cid = 0
 while True:
+    i_cid += 1
     total_consumo, total_pessoas = 0, 0
     dict_consumo = defaultdict(int)
 
-    for _ in range(qt_imoveis):
-        pessoas, consumo = map(int, input().split())
+    for pessoas, consumo in (map(int, input().split()) for _ in range(qt_imoveis)):
         total_pessoas += pessoas
         total_consumo += consumo
         dict_consumo[consumo // pessoas] += pessoas
 
-    qt_imoveis = int(input())
-    if qt_imoveis:
-        lista_prints.append("Cidade# {0}:\n"
-                            "{1}\n"
-                            "Consumo medio: {2:.2f} m3.\n".
-                            format(index,
-                                   " ".join("{0}-{1}".format(dict_consumo[cons], cons)
-                                            for cons in sorted(dict_consumo)),
-                                   int(total_consumo * 100 / total_pessoas) / 100))
-        index += 1
-    else:
-        lista_prints.append("Cidade# {0}:\n"
-                            "{1}\n"
-                            "Consumo medio: {2:.2f} m3.\n".
-                            format(index,
-                                   " ".join("{0}-{1}".format(dict_consumo[cons], cons)
-                                            for cons in dict_consumo),
-                                   int(total_consumo * 100 / total_pessoas) / 100))
-        break
+    print("Cidade# {0}:\n"
+          "{1}\n"
+          "Consumo medio: {2:.2f} m3.".
+          format(i_cid,
+                 " ".join("{0}-{1}".format(dict_consumo[cons], cons) for cons in sorted(dict_consumo)),
+                 int(total_consumo * 100 / total_pessoas) / 100))
 
-print("\n".join(lista_prints))
+    qt_imoveis = int(input())
+    if not qt_imoveis:
+        break
+    else:
+        print("")
