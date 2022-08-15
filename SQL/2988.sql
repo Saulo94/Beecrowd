581dc5affaef1978b23e2b24fc810abe6945d306
Saulo Roberto <sauloroberto1000@hotmail.com>
2022-08-15 16:08:17 -0300

    problema 2988 resolvido

--- SQL/2988.sql
+++ SQL/2988.sql
@@ -1,54 +1,55 @@
 WITH tmp AS 
-(SELECT teams.name as name_tmp,
+(
+SELECT teams_tmp.name,
 	   (SELECT COUNT(*)
         FROM teams, matches
         WHERE teams.id = matches.team_2
-        AND teams.name = name_tmp) +
+        AND teams.name = teams_tmp.name) +
        (SELECT COUNT(*)
         FROM teams, matches
         WHERE teams.id = matches.team_1
-        AND teams.name = name_tmp) AS matches_tmp,
+        AND teams.name = teams_tmp.name) AS matches_tmp,
        (SELECT COUNT(*)
         FROM teams, matches
         WHERE teams.id = matches.team_1
-       	AND teams.name = name_tmp
+       	AND teams.name = teams_tmp.name
         AND matches.team_1_goals > matches.team_2_goals) +
        (SELECT COUNT(*)
         FROM teams, matches
         WHERE teams.id = matches.team_2
-       	AND teams.name = name_tmp
+       	AND teams.name = teams_tmp.name
         AND matches.team_2_goals > matches.team_1_goals) AS victories,
        (SELECT COUNT(*)
         FROM teams, matches
         WHERE teams.id = matches.team_1
-       	AND teams.name = name_tmp
+       	AND teams.name = teams_tmp.name
         AND matches.team_1_goals < matches.team_2_goals) +
        (SELECT COUNT(*)
         FROM teams, matches
         WHERE teams.id = matches.team_2
-       	AND teams.name = name_tmp
+       	AND teams.name = teams_tmp.name
         AND matches.team_2_goals < matches.team_1_goals) AS defeats,
        (SELECT COUNT(*)
         FROM teams, matches
         WHERE teams.id = matches.team_1
-       	AND teams.name = name_tmp
+       	AND teams.name = teams_tmp.name
         AND matches.team_1_goals = matches.team_2_goals) +
        (SELECT COUNT(*)
         FROM teams, matches
         WHERE teams.id = matches.team_2
-       	AND teams.name = name_tmp
+       	AND teams.name = teams_tmp.name
         AND matches.team_2_goals = matches.team_1_goals) AS draws
-FROM teams)
+FROM teams AS teams_tmp
+)
 
-SELECT name_tmp as name, 
-	   matches_tmp as matches,
-       victories, 
-       defeats, 
+SELECT teams_tmp2.name,
+	   matches_tmp AS matches,
+       victories,
+       defeats,
        draws,
-	   (SELECT victories * 3 + draws
-        FROM tmp
-        WHERE name_tmp = name) AS score
-FROM tmp
+      (SELECT victories * 3 + draws
+       FROM tmp WHERE tmp.name = teams_tmp2.name) AS score
+FROM tmp as teams_tmp2
 ORDER BY score DESC, name;
 
 $$ language plpgsql;
