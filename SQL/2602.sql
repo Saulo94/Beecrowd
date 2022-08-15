8b2f40096040391d60332f9fc46133f582a8aff1
Saulo Roberto <sauloroberto1000@hotmail.com>
2022-08-14 22:43:06 -0300

    teste

--- SQL/2602.sql
+++ SQL/2602.sql
@@ -1,1 +1,11 @@
+/*
+// pgAgent - PostgreSQL Tools
+// 
+// Copyright (C) 2002 - 2021, The pgAdmin Development Team
+// This software is released under the PostgreSQL Licence
+//
+// pgagent.sql - pgAgent tables and functions
+//
+*/
+
 SELECT name FROM customers WHERE state = 'RS';
